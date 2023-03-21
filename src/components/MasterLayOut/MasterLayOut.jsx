fde3ec6688dda1caee34a709c93645bba2582c7f
abdallah608 <abdallahhassanshaaban@gmail.com>
2023-03-21 16:08:08 +0200

    first commit

--- src/components/MasterLayOut/MasterLayOut.jsx
+++ src/components/MasterLayOut/MasterLayOut.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
-import NavBar from '../../NavBar/NavBar.jsx'
+import NavBar from '../NavBar/NavBar.jsx'
 import Footer from '../Footer/Footer.jsx'
 import Header from '../Header/Header.jsx'
 
